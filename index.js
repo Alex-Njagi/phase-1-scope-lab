072533027dbfbe869deab3a7e739593cbd5309dd
Alex Njagi <1992anjagi@gmail.com>
2023-03-21 16:20:51 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = `bob`;
+
+function upperCaseCustomerName (){
+    return customerName.toUpperCase();
+}
+var customerName = upperCaseCustomerName();
+
+function setBestCustomer() {
+    var bestCustomer = `not bob`;
+}
+setBestCustomer();
+
+function overwriteBestCustomer(newBestCustomer) {
+    var bestCustomer = newBestCustomer;
+}
+newBestCustomer();
+
+const leastFavoriteCustomer = `Aria`;
+function changeLeastFavoriteCustomer(leastFavoriteCustomer) {
+    const leastFavoriteCustomer = leastFavoriteCustomer;
+    return leastFavoriteCustomer;
+}
+leastFavoriteCustomer();
